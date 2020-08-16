77233b669d5615b841a2e9a67271407a74a322ea
Oliver Mason <omason@protonmail.com>
2020-08-17 00:23:12 +1000

    Add error checking

--- index.py
+++ index.py
@@ -11,6 +11,7 @@ day = date.day
 # Set url:
 url = f"https://www.xe.com/currencytables/?from=AUD&date={year}-{month}-{day}"
 
+print(url)
 # The "result" sends a request to the url and returns the http data from the target webpage, type <class 'requests.models.Response'>
 result = requests.get(url)
 
@@ -19,52 +20,91 @@ soup = bs4.BeautifulSoup(result.text, "lxml")
 
 title = soup.select('title')[0].getText()                # Set title to match the <title> tag on the webpage
 
-'''
-BeautifulSoup selector cheatsheet:
-    soup.select('div')                  - Selects ALL based on tag type 
-    soup.select('#special_id)           - Selects ALL elemments with the named id
-    soup.select('.class_name')           - Select HTML elements by class name
-    soup.select('div element_name')     - Select all instances of element_name within the named div
-    soup.select('div > element_name')   - As above but with nothing in between
-'''
-
 # Set the rates to be returned in the terminal:
 
 aud_to_usd = soup.select('.historicalRateTable-rateHeader')[3].getText()
-usd_to_aud = soup.select('.historicalRateTable-rateHeader')[2].getText()
+usd_to_aud = float(soup.select('.historicalRateTable-rateHeader')[2].getText())
 
-aud_to_nzd = soup.select('.historicalRateTable-rateHeader')[25].getText()
-nzd_to_aud = soup.select('.historicalRateTable-rateHeader')[24].getText()
+aud_to_nzd = float(soup.select('.historicalRateTable-rateHeader')[25].getText())
+nzd_to_aud = float(soup.select('.historicalRateTable-rateHeader')[24].getText())
 
-aud_to_jpy = soup.select('.historicalRateTable-rateHeader')[21].getText()
-jpy_to_aud = soup.select('.historicalRateTable-rateHeader')[20].getText()
+aud_to_jpy = float(soup.select('.historicalRateTable-rateHeader')[21].getText())
+jpy_to_aud = float(soup.select('.historicalRateTable-rateHeader')[20].getText())
 
-aud_to_eur = soup.select('.historicalRateTable-rateHeader')[5].getText()
-eur_to_aud = soup.select('.historicalRateTable-rateHeader')[4].getText()
+aud_to_eur = float(soup.select('.historicalRateTable-rateHeader')[5].getText())
+eur_to_aud = float(soup.select('.historicalRateTable-rateHeader')[4].getText())
 
-aud_to_rub = soup.select('.historicalRateTable-rateHeader')[63].getText()
-rub_to_aud = soup.select('.historicalRateTable-rateHeader')[62].getText()
+aud_to_rub = float(soup.select('.historicalRateTable-rateHeader')[63].getText())
+rub_to_aud = float(soup.select('.historicalRateTable-rateHeader')[62].getText())
 
-aud_to_col = soup.select('.historicalRateTable-rateHeader')[79].getText()
-col_to_aud = soup.select('.historicalRateTable-rateHeader')[78].getText()
+aud_to_col = float(soup.select('.historicalRateTable-rateHeader')[79].getText())
+col_to_aud = float(soup.select('.historicalRateTable-rateHeader')[78].getText())
 
+# Identify any extraordinary FX movements - threshold set at 5% variance from initial start:
 
+variance_result = "passed"      # Default if variance_check does not return an error
 
-print("aud_to_usd should be 1.3943009086, and is: ", aud_to_usd)           
-print("usd_to_aud should be 0.7172052990, and is: ", usd_to_aud)
+def variance_check():
+    print("Variance check is running")
+    if (aud_to_usd / 1.3943009086) > 1.05 or (aud_to_usd / 1.3943009086) < 0.95:
+        variance_result = "FAILED - check AUD:USD rate"
+    
+    if (usd_to_aud / 0.7172052990) > 1.05 or (usd_to_aud / 0.7172052990) < 0.95:
+        variance_result = "FAILED - check USD:AUD rate"
 
-print("aud_to_nzd should be 0.9119575285, and is: ", aud_to_nzd)
-print("nzd_to_aud should be 1.0965422936, and is: ", nzd_to_aud)
+    if (aud_to_nzd / 0.9119575285) > 1.05 or (aud_to_nzd / 0.9119575285) < 0.95:
+        variance_result = "FAILED - check AUD:NZD rate"
 
-print("aud_to_jpy should be 0.0130836400, and is: ", aud_to_jpy)
-print("jpy_to_aud should be 76.4313295293, and is: ", jpy_to_aud)
+    if (nzd_to_aud / 1.0965422936) > 1.05 or (nzd_to_aud / 1.0965422936) < 0.95:
+        variance_result = "FAILED - check NZD:AUD rate"
 
-print("aud_to_eur should be 1.6511313406, and is: ", aud_to_eur)
-print("eur_to_aud should be 0.6056453387, and is: ", eur_to_aud)
+    if (aud_to_jpy / 0.0130836400) > 1.05 or (aud_to_jpy / 0.0130836400) < 0.95:
+        variance_result = "FAILED - check AUD:JPY rate"
 
-print("aud_to_rub should be 0.0191310545, and is: ", aud_to_rub)
-print("rub_to_aud should be 52.2710339969, and is: ", rub_to_aud)
+    if (jpy_to_aud / 76.4313295293) > 1.05 or (jpy_to_aud / 76.4313295293) < 0.95:
+        variance_result = "FAILED - check JPY:AUD rate"
 
-print("aud_to_col should be 0.0003675975, and is: ", aud_to_col)
-print("col_to_aud should be 2720.3668401739, and is: ", col_to_aud)
+    if (aud_to_eur / 1.6511313406) > 1.05 or (aud_to_eur / 1.6511313406) < 0.95:
+        variance_result = "FAILED - check AUD:EUR rate"
 
+    if (eur_to_aud / 0.6056453387) > 1.05 or (eur_to_aud / 0.6056453387) < 0.95:
+        variance_result = "FAILED - check EUR:AUD rate"
+
+    if (aud_to_rub / 0.0191311081) > 1.05 or (aud_to_rub / 0.0191311081) < 0.95:
+        variance_result = "FAILED - check AUD:RUB rate"
+
+    if (rub_to_aud / 52.2708876046) > 1.05 or (rub_to_aud / 52.2708876046) < 0.95:
+        variance_result = "FAILED - check RUB:AUD rate"
+
+    if (aud_to_col / 0.0003675963) > 1.05 or (aud_to_col / 0.0003675963) < 0.95:
+        variance_result = "FAILED - check AUD:COL rate"
+
+    if (col_to_aud / 2720.3758756422) > 1.05 or (col_to_aud / 2720.3758756422) < 0.95:
+        variance_result = "FAILED - check COL:AUD rate"
+
+variance_check()
+print("Variance check returns", variance_check)
+
+view = f'''
+    AUD:USD = {aud_to_usd}           
+    USD:AUD = {usd_to_aud}              
+
+    AUD:NZD = {aud_to_nzd}
+    NZD:AUD = {nzd_to_aud}
+
+    AUD:JPY = {aud_to_jpy}
+    JPY:AUD = {jpy_to_aud}
+
+    AUD:EUR = {aud_to_eur}
+    EUR:AUD = {eur_to_aud}
+
+    AUD:RUB = {aud_to_rub}
+    RUB:AUD = {rub_to_aud}
+
+    AUD:COL = {aud_to_col}
+    COL:AUD = {col_to_aud}
+
+    The FX variance sense checks have {variance_result}...
+'''
+
+print(view)
