aa1d60f8e7e915f570468ad00978a5e2a4f5e583
Oliver Mason <omason@protonmail.com>
2020-08-17 15:03:23 +1000

    De-bug variance_check function

--- index.py
+++ index.py
@@ -6,7 +6,7 @@ import bs4                                  # Required to parse through http res
 date = datetime.datetime.now()
 year = date.year
 month = date.strftime("%m") 
-day = date.day
+day = date.day - 1                          # - 1 as current day rates are sometimes not available until evening Australia time
 
 # Set url:
 url = f"https://www.xe.com/currencytables/?from=AUD&date={year}-{month}-{day}"
@@ -22,7 +22,7 @@ title = soup.select('title')[0].getText()                # Set title to match th
 
 # Set the rates to be returned in the terminal:
 
-aud_to_usd = soup.select('.historicalRateTable-rateHeader')[3].getText()
+aud_to_usd = float(soup.select('.historicalRateTable-rateHeader')[3].getText())
 usd_to_aud = float(soup.select('.historicalRateTable-rateHeader')[2].getText())
 
 aud_to_nzd = float(soup.select('.historicalRateTable-rateHeader')[25].getText())
@@ -42,48 +42,44 @@ col_to_aud = float(soup.select('.historicalRateTable-rateHeader')[78].getText())
 
 # Identify any extraordinary FX movements - threshold set at 5% variance from initial start:
 
-variance_result = "passed"      # Default if variance_check does not return an error
-
 def variance_check():
-    print("Variance check is running")
     if (aud_to_usd / 1.3943009086) > 1.05 or (aud_to_usd / 1.3943009086) < 0.95:
-        variance_result = "FAILED - check AUD:USD rate"
+        return "FAILED - check AUD:USD rate"
     
     if (usd_to_aud / 0.7172052990) > 1.05 or (usd_to_aud / 0.7172052990) < 0.95:
-        variance_result = "FAILED - check USD:AUD rate"
+        return "FAILED - check USD:AUD rate"
 
     if (aud_to_nzd / 0.9119575285) > 1.05 or (aud_to_nzd / 0.9119575285) < 0.95:
-        variance_result = "FAILED - check AUD:NZD rate"
+        return "FAILED - check AUD:NZD rate"
 
     if (nzd_to_aud / 1.0965422936) > 1.05 or (nzd_to_aud / 1.0965422936) < 0.95:
-        variance_result = "FAILED - check NZD:AUD rate"
+        return "FAILED - check NZD:AUD rate"
 
     if (aud_to_jpy / 0.0130836400) > 1.05 or (aud_to_jpy / 0.0130836400) < 0.95:
-        variance_result = "FAILED - check AUD:JPY rate"
+        return "FAILED - check AUD:JPY rate"
 
     if (jpy_to_aud / 76.4313295293) > 1.05 or (jpy_to_aud / 76.4313295293) < 0.95:
-        variance_result = "FAILED - check JPY:AUD rate"
+        return "FAILED - check JPY:AUD rate"
 
     if (aud_to_eur / 1.6511313406) > 1.05 or (aud_to_eur / 1.6511313406) < 0.95:
-        variance_result = "FAILED - check AUD:EUR rate"
+        return "FAILED - check AUD:EUR rate"
 
     if (eur_to_aud / 0.6056453387) > 1.05 or (eur_to_aud / 0.6056453387) < 0.95:
-        variance_result = "FAILED - check EUR:AUD rate"
+        return "FAILED - check EUR:AUD rate"
 
     if (aud_to_rub / 0.0191311081) > 1.05 or (aud_to_rub / 0.0191311081) < 0.95:
-        variance_result = "FAILED - check AUD:RUB rate"
+        return "FAILED - check AUD:RUB rate"
 
     if (rub_to_aud / 52.2708876046) > 1.05 or (rub_to_aud / 52.2708876046) < 0.95:
-        variance_result = "FAILED - check RUB:AUD rate"
+        return "FAILED - check RUB:AUD rate"
 
     if (aud_to_col / 0.0003675963) > 1.05 or (aud_to_col / 0.0003675963) < 0.95:
-        variance_result = "FAILED - check AUD:COL rate"
+        return "FAILED - check AUD:COL rate"
 
     if (col_to_aud / 2720.3758756422) > 1.05 or (col_to_aud / 2720.3758756422) < 0.95:
-        variance_result = "FAILED - check COL:AUD rate"
-
-variance_check()
-print("Variance check returns", variance_check)
+        return "FAILED - check COL:AUD rate"
+    else:
+        return "passed"
 
 view = f'''
     AUD:USD = {aud_to_usd}           
@@ -104,7 +100,7 @@ view = f'''
     AUD:COL = {aud_to_col}
     COL:AUD = {col_to_aud}
 
-    The FX variance sense checks have {variance_result}...
+    The FX variance sense checks have {variance_check()}...
 '''
 
 print(view)
